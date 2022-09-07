17aa446c13576304d7483381151e34efbc4b478f
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-07 02:41:20 -0500

    Add sing_up/sign_in styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :config_params_sanitizer, if: :devise_controller?
+
+  def config_params_sanitizer
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def index; end
 end
